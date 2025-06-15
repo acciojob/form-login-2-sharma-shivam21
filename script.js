7a83cc0f326cf9cccd8e8cdd4a399ca8292fe7a2
sharma-shivam21 <s3860762@gmail.com>
2025-06-15 16:00:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+document.getElementById("loginForm").addEventListener("submit", function(e) {
+      e.preventDefault(); // Prevent page reload
+
+      const form = e.target;
+      const firstName = form["First Name"].value;
+      const lastName = form["Last Name"].value;
+      const phoneNumber = form["Phone Number"].value;
+      const email = form["Email ID"].value;
+
+      const message = `First Name: ${firstName} Last Name: ${lastName} Phone Number: ${phoneNumber} Email ID: ${email}`;
+      alert(message);
+    });
+  </script>
